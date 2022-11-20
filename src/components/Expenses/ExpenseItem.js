1e3e48c663460a85c616cd4b274f69bde5a33344
Ravinath Fernando <ravinath_fernando@combank.net>
2022-11-21 01:53:48 +0530

    state

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,31 @@
+
+import React, { useState } from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+
+  const [title , setTitle] = useState(props.title); // arrray decomposistion 
+
+  const clickHandler = () => {
+    setTitle('Updated');
+    console.log('clicked !!!!!');
+    
+  }
+
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
