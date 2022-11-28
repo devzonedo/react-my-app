20e7f0a67b8ddf0a15604cb6767f1487723371aa
Ravinath Fernando <ravinath_fernando@combank.net>
2022-11-28 16:39:53 +0530

    assignment

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,26 @@
+import React, { useState } from 'react';
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props){
-    return(<Card className="expenses"><ExpenseItem 
+
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = (selectedYear) => {
+        console.log('Expenses.ja');
+        console.log(selectedYear);
+        setFilteredYear(selectedYear);
+    }
+
+
+    return(
+    <div>
+    
+    <Card className="expenses">
+    <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        <ExpenseItem 
         title={props.expenses[0].title} 
         amount={props.expenses[0].amount}
         date={props.expenses[0].date}></ExpenseItem>
@@ -21,6 +38,7 @@ function Expenses(props){
   <ExpenseItem 
         title={props.expenses[3].title} 
         amount={props.expenses[3].amount}
-        date={props.expenses[3].date}></ExpenseItem></Card>);
+        date={props.expenses[3].date}></ExpenseItem></Card></div>);
+
 }
 export default Expenses;
